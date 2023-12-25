2600137786653e08bdb8eae31a40980b60a8e391
JatinVira <jatinvira666@gmail.com>
2023-12-25 15:36:16 +0530

    add live feed with roi to start cam

--- fg_gazebo_example/scripts/start_camera.py
+++ fg_gazebo_example/scripts/start_camera.py
@@ -24,6 +24,8 @@ rospy.init_node("camera", anonymous=True)
 # Set the loop rate
 rate = rospy.Rate(10)
 
+region_of_interest = (20, 104, 168, 191)
+
 # Loop until ROS is shutdown
 while not rospy.is_shutdown():
     # Capture a frame from the camera
@@ -37,6 +39,22 @@ while not rospy.is_shutdown():
 
     # Publish the ROS image
     pub.publish(ros_image)
+
+    # Resize the image
+    resized_image = cv2.resize(frame, (640, 480))
+
+    # Draw a Rectangle on the image of the region of interest
+    cv2.rectangle(
+        resized_image,
+        (region_of_interest[0], region_of_interest[1]),
+        (region_of_interest[2], region_of_interest[3]),
+        (0, 255, 0),
+        2,
+    )
+
+    # Show the image
+    cv2.imshow("Camera_Live_Feed", resized_image)
+    cv2.waitKey(1)
 
     # Sleep for the remainder of the loop
     rate.sleep()
